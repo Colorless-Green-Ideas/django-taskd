69847e47c8b348080586d5af006328724431bbf4
amelia sabine <amelia.sabine@riseup.net>
2016-07-12 11:07:25 -0700

    fix typos in setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ import os
 from setuptools import setup
 
 README = open('README.md').read()
-LICENSE = open('LICENSE.txt').read()
+LICENSE = open('LICENSE.md').read()
 
 setup(
         name="django-taskd",
-        version="0.0.0-alpha"
+        version="0.0.0-alpha",
         url="https://github.com/asabine/django-taskd",
         author="amelia sabine",
-        author_email="amelia.sabine@getpizza.cat"
+        author_email="amelia.sabine@getpizza.cat",
         license=LICENSE,
-        py_modules=['taskdj']
+        py_modules=['taskdj'],
         description="""A taskd implementation for the Django web framework. Enables django applications to easily integrate
-        tasks with Taskwarrior."""
+        tasks with Taskwarrior.""",
         long_description=README,
         install_requires=['django', 'taskc'],
     )
