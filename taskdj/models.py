c6c2b44785a96a2358c1d2815acc40140a655b50
jrabbit <jackjrabbit@gmail.com>
2016-10-06 17:48:54 -0400

    oops that doesn't work there.

--- taskdj/models.py
+++ taskdj/models.py
@@ -3,7 +3,7 @@ import json
 import uuid
 
 from django.db import models
-
+from django.core.validators import MinLengthValidator
 
 class BaseTaskdUser(models.Model):
     """
@@ -43,7 +43,7 @@ class BaseTask(models.Model):
     status = models.CharField(max_length=9, blank=False, choices=statuses, default="pending")
     uuid = models.UUIDField(blank=False, unique=True, default=uuid.uuid4)
     entry = models.DateTimeField(auto_now_add=True, editable=False)
-    description = models.TextField(min_length=1)
+    description = models.TextField(validators=[MinLengthValidator(1)])
     start = models.DateTimeField(blank=True, null=True)
     end = models.DateTimeField(blank=True, null=True)
     due = models.DateTimeField(blank=True, null=True)
